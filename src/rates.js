95ca938a9ead43d9e69a723c9da42a443cd423eb
Maria Lafutkina <lafutkina.masha@yandex.ru>
2024-02-09 02:18:33 +0300

    feature: to add function for parse from ligovka

--- src/rates.js
+++ src/rates.js
@@ -1,10 +1,10 @@
 import axios from "axios";
 import puppeteer from "puppeteer";
 
-export async function getForexRate() {
-  let message = '';
+async function getForexRate() {
+  let message = "";
   try {
-    const url = 'https://www.profinance.ru/';
+    const url = "https://www.profinance.ru/";
     const browser = await puppeteer.launch();
     const page = await browser.newPage();
 
@@ -14,7 +14,10 @@ export async function getForexRate() {
 
     await page.waitForFunction(
       () => {
-        return document.querySelector('.curs td:nth-child(2)').textContent.trim() !== "0.00";
+        return (
+          document.querySelector(".curs td:nth-child(2)").textContent.trim() !==
+          "0.00"
+        );
       },
       { timeout: 30000 }
     );
@@ -25,7 +28,7 @@ export async function getForexRate() {
 
       rows.forEach((row) => {
         const titleElement = row.querySelector("td > a");
-        const priceElement = row.querySelector('td:nth-child(2)');
+        const priceElement = row.querySelector("td:nth-child(2)");
 
         if (titleElement && priceElement) {
           rates.push({
@@ -43,19 +46,77 @@ export async function getForexRate() {
     const numberRegex = /^[+-]?(\d+(\.\d+)?|\.\d+)(%)?$/;
 
     //select only what need
-    const filteredRates = rates.filter(rate =>
-      numberRegex.test(rate.price) &&
-      (rate.title === "USD/RUB" ||
-      rate.title === "EUR/RUB" ||
-      rate.title === "CNY/RUB" ||
-      rate.title === "EUR/USD")
+    const filteredRates = rates.filter(
+      (rate) =>
+        numberRegex.test(rate.price) &&
+        (rate.title === "USD/RUB" ||
+          rate.title === "EUR/RUB" ||
+          rate.title === "CNY/RUB" ||
+          rate.title === "EUR/USD")
     );
 
-    // Форматируем сообщение
-    message = filteredRates.map(rate => `${rate.title}(Forex покупка) - ${rate.price}`).join('\n');
-
+    message = filteredRates
+      .map((rate) => `${rate.title}(Forex покупка) - ${rate.price}`)
+      .join("\n");
   } catch (error) {
     console.error(`Ошибка при получении курсов Forex: ${error.message}`);
+  }
+  return message;
+}
+
+async function getLigRate() {
+  let message = "";
+  try {
+    const url = "https://ligovka.ru/pda/";
+    const browser = await puppeteer.launch();
+    const page = await browser.newPage();
+
+    await page.goto(url, {
+      waitUntil: "domcontentloaded",
+    });
+
+    const rates = await page.evaluate(() => {
+      const containers = document.querySelectorAll(".currency_contaner");
+      const rates = Array.from(containers).map((container) => {
+        const title = container
+          .querySelector(".currency_name")
+          .textContent.trim();
+        const tableRows = container.querySelectorAll(
+          "table.currency_table tbody tr"
+        );
+        const firstRow = tableRows[0];
+
+        const buyPrice = firstRow.cells[1].textContent.trim();
+        const sellPrice = firstRow.cells[2].textContent.trim();
+
+        return {
+          title,
+          buyPrice,
+          sellPrice,
+        };
+      });
+
+      return rates;
+    });
+
+    await browser.close();
+
+    const filteredRates = rates.filter(
+      (rate) =>
+        rate.title === "USD" ||
+        rate.title === "EUR" ||
+        rate.title === "EUR/USD"  ||
+        rate.title === "CNY"
+    );
+
+    message = filteredRates
+      .map(
+        (rate) =>
+          `${rate.title}(Покупка): ${rate.buyPrice}\n${rate.title}(Продажа): ${rate.sellPrice}`
+      )
+      .join("\n");
+  } catch (error) {
+    console.error(`Ошибка при получении курсов валют: ${error.message}`);
   }
   return message;
 }
@@ -65,29 +126,30 @@ export async function getCryptoRate() {
   let messages = [];
   try {
     const markets = ["btcusdt", "btcrub", "ethusdt", "usdteur"];
-    const responses = await Promise.all(markets.map(market =>
-      axios.get(`https://garantex.org/api/v2/depth?market=${market}`)
-    ));
+    const responses = await Promise.all(
+      markets.map((market) =>
+        axios.get(`https://garantex.org/api/v2/depth?market=${market}`)
+      )
+    );
 
     messages = responses.map((res, index) => {
       const market = markets[index].toUpperCase();
       const data = res.data["asks"][0];
       return `${market} - ${data.price}`;
     });
-
   } catch (error) {
     console.error(`Ошибка при получении курсов криптовалют: ${error.message}`);
   }
-  return messages.join('\n');
+  return messages.join("\n");
 }
 
 // Теперь напишем функцию sendCombinedMessage
 export async function sendCombinedMessage(ctx) {
   const cryptoRates = await getCryptoRate();
   const forexRates = await getForexRate();
-
+  const ligRates = await getLigRate();
   // Соединяем отформатированные сообщения
-  const combinedMessage = `\`\`\`\n${cryptoRates}\n\n${forexRates}\`\`\``;
+  const combinedMessage = `\`\`\`\n${cryptoRates}\n\n${ligRates}\n\n${forexRates}\`\`\``;
 
   // Отправляем соединенное сообщение
   ctx.reply(combinedMessage, { parse_mode: "Markdown" });
