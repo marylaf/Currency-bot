85eaa957ca6b4722b815e175dcc60ec42fe9386a
Maria Lafutkina <lafutkina.masha@yandex.ru>
2024-02-13 00:11:33 +0300

    fixed: to add headless for puppeteer

--- src/rates.js
+++ src/rates.js
@@ -9,7 +9,7 @@ async function getForexRate() {
   try {
     const url = "https://www.profinance.ru/";
     const browser = await puppeteer.launch({
-      // headless: true,
+      headless: true,
       args: ['--no-sandbox', '--disable-setuid-sandbox']
    });
     const page = await browser.newPage();
